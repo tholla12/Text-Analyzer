24cb5edd39986da52a3a2ce0bba1a1f8b475d5ec
Tola Bello <tolabello4@gmail.com>
2023-03-30 11:19:44 -0700

    add javascript file user interface one

--- js23/script23.js
+++ js23/script23.js
@@ -56,4 +56,25 @@ function offensiveWords(text) {
   return offensiveTag + "</p>";
 }
 
+// UI Logic
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function (element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
+
 
