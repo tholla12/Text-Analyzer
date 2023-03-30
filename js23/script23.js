a7b9fc22732e1faa27a8add1d74f1c4aaadb64f4
Tola Bello <tolabello4@gmail.com>
2023-03-30 11:14:05 -0700

    add javascript file business logic one

--- js23/script23.js
+++ js23/script23.js
@@ -4,3 +4,19 @@ function noInputtedWord(word, text) {
   return ((text.trim().length === 0) || (word.trim().length === 0));
 }
 
+// Business Logic
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function (element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
